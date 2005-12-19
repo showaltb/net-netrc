5078d94e156ec6f942ca70cf8904c1ea5d2c47a7
Bob Showalter <showaltb@gmail.com>
2005-12-19 16:25:52 +0000

    Initial add
    git-svn-id: svn+ssh://rubyforge.org/var/svn/net-netrc/trunk/net-netrc@35 1c57dcda-0a1e-0410-ae01-dcdfb7d97a71

---
+++ lib/net/ftp-netrc.rb
@@ -0,0 +1,76 @@
+# = net/ftp-netrc.rb - Net::FTP / Net::Netrc integration
+#
+# Copyright (c) 2005 Robert J. Showalter
+#
+# This library is distributed under the terms of the Ruby license.
+# You may freely distribute or modify this library.
+#
+# This module extends the Net::FTP#login method to use Net::Netrc
+# to lookup login information if a nil username is passed.
+#
+# Example:
+#
+#   require 'net/ftp-netrc'     # (brings in net/ftp and net/netrc)
+#
+#   ftp = Net::FTP.new('myhost')
+#   ftp.login(nil)
+#   ftp.last_response
+#   => 230 User myuser logged in.
+#
+# $Id$
+
+require 'net/ftp'
+require 'net/netrc'
+
+module Net
+
+  class FTP
+
+    alias_method :orig_connect, :connect  # :nodoc:
+    alias_method :orig_login, :login      # :nodoc:
+
+    # cache host name for later use by login
+    def connect(host, port = FTP_PORT) # :nodoc:
+      @host = host
+      orig_connect(host, port)
+    end
+
+    #
+    # Logs in to the remote host. The session must have been previously
+    # connected. 
+    #
+    # If +user+ is nil, Net::Netrc#locate is used to lookup login information
+    # based on the host name supplied to Net::Netrc#connect.
+    #
+    # If +user+ is the string "anonymous" and the +password+ is nil, a password
+    # of user@host is synthesized. If the +acct+ parameter is not nil, an FTP
+    # ACCT command is sent following the successful login. Raises an exception
+    # on error (typically Net::FTPPermError).
+    #
+    # Example:
+    #
+    #   require 'net/ftp-netrc'     # (brings in net/ftp and net/netrc)
+    #
+    #   ftp = Net::FTP.new('myhost')
+    #   ftp.login(nil)
+    #   ftp.last_response
+    #   => 230 User myuser logged in.
+    #
+    def login(user = "anonymous", passwd = nil, acct = nil)
+      if user.nil?
+        rc = Net::Netrc.locate(@host)
+        if rc
+          user = rc.login
+          passwd = rc.password
+          acct = rc.account
+        else
+          user = ''
+          passwd = ''
+        end
+      end
+      orig_login(user, passwd, acct)
+    end
+
+  end
+
+end
