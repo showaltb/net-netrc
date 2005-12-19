3f021dadd42d0e1bcf4a3a514b40ae64e1c8aa7a
Bob Showalter <showaltb@gmail.com>
2005-12-19 16:42:01 +0000

    0.2.0 release
    git-svn-id: svn+ssh://rubyforge.org/var/svn/net-netrc/trunk/net-netrc@38 1c57dcda-0a1e-0410-ae01-dcdfb7d97a71

--- lib/net/netrc.rb
+++ lib/net/netrc.rb
@@ -79,7 +79,7 @@ module Net
   class Netrc
 
     VERSION_MAJOR = 0
-    VERSION_MINOR = 1
+    VERSION_MINOR = 2
     VERSION_PATCH = 0
     VERSION = "#{VERSION_MAJOR}.#{VERSION_MINOR}.#{VERSION_PATCH}"
 
